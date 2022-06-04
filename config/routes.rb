98f76ef67ff7529de9cd6ac8d4ad95c71c5353aa
pythagoras-yamamoto <yamamo8533@gmail.com>
2022-06-04 15:41:49 +0900

    モデルの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :restaurant do
+        resources :foods, only: %i[index]
+      end
+      resources :line_foods, only: %i[index create]
+      put 'line_foods/replace', to: 'line_foods#replace'
+      resources :orders, only: %i[create]
+    end
+  end
 end
